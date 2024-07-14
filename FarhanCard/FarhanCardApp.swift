0766cb971c702c9d28a1ff1d75e1b0113364cddf
farhanfadhilah <farhanfadhilah87@gmail.com>
2024-07-14 17:22:54 +0700

    Initial Commit

---
+++ FarhanCard/FarhanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarhanCardApp.swift
+//  FarhanCard
+//
+//  Created by Farhan Fadhilah on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FarhanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
